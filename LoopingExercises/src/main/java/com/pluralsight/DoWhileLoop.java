4e821c3ba60e448941e1432c41fc7ca85f3a4c30
karina-krupodior <karinakrupodior@gmail.com>
2025-04-18 17:35:11 -0500

    Completed LoopingExercises with While, DoWhile, and For loops

---
+++ LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("I love Java");
+            i++;
+        }while(i < 5);
+    }
+
+}
+
+//Create another class named DoWhileLoop. Add a new main() method and
+//print "I love Java" again, 5 times, but this use a do/while loop
---
+++ LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ForLoop {
+    public static void main(String[] args) throws InterruptedException {
+
+                for(int i = 10; i >= 1; i--) {
+                    Thread.sleep(1000);
+                    if(i == 1) {
+                        System.out.println("Launch!" + i);
+                    }
+
+                }
+    }
+}
+
+
